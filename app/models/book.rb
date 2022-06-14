ff721510a889375b8c4fca7df3e0e007b82086c7
Meri <mgogi13@freeuni.edu.ge>
2022-06-14 22:27:38 +0400

    feat: add dependent destroy for comments on books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,8 @@
 class Book < ApplicationRecord
   has_many :comments
+  before_destroy do
+    comments.each(&:destroy)
+  end
 
   validates :author, presence: true, length: {minimum: 3}
   validates :title, presence: true, length: {minimum: 3}
